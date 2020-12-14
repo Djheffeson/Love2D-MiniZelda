da9bc94deb8c0db12a9f2da3670469be50be561c
Djheffeson <djheffeson@outlook.com>
2020-12-14 12:04:10 -0300

    finished the "game ending"

--- mini_zelda/src/endGame.lua
+++ mini_zelda/src/endGame.lua
@@ -4,10 +4,25 @@ function endGameStart()
     endGame.start = false
     endGame.timer = 0
     endGame.moveY = 0.3
+
+    endGame.anim = Player.deathAnimation
+    endGame.animX = 52
+    endGame.animY = 168
+    endGame.animSpeed = 80
 end
 
 function endGame:update(dt)
+
     if endGame.start == true then
+
+        endGame.anim:update(dt)
+        if endGame.animX <= 48 then
+            endGame.animSpeed = 80
+        elseif endGame.animX >= 192 then
+            endGame.animSpeed = -80
+        end
+        endGame.animX = endGame.animX + endGame.animSpeed * dt
+
         endGame.timer = endGame.timer + 1 * dt
         if endGame.timer <= 8 then
             sounds.getShard:play()
@@ -26,7 +41,6 @@ function endGame:update(dt)
 
             writeHere(7, 'this ends the story.', 51, 208, 0.133)
         elseif guiY >= -VIRTUAL_HEIGHT then
-            print(guiY)
             guiY = guiY - endGame.moveY
         else
             if love.keyboard.isDown('return') then
@@ -34,17 +48,20 @@ function endGame:update(dt)
             end
         end
     end
-    
 end
 
 function endGame:draw()
-    love.graphics.print('this is was my final\n  project for CS50!', 50, 38+VIRTUAL_HEIGHT+guiY)
-    love.graphics.print('made in love2d', 74, 62+VIRTUAL_HEIGHT+guiY)
-    love.graphics.setColor(88/255, 225/255, 49/255, 1)
-    love.graphics.print('coded by djheffeson', 54, 90+VIRTUAL_HEIGHT+guiY)
-    love.graphics.setColor(179/255, 48/255, 33/255, 1)
-    love.graphics.print('credits: nintendo', 62, 106+VIRTUAL_HEIGHT+guiY)
-    love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.print('thanks for playing!', 56, 170+VIRTUAL_HEIGHT+guiY)
-    love.graphics.print('press enter to close the game', 12, 186+VIRTUAL_HEIGHT+guiY)
+    if endGame.start == true then
+        love.graphics.print('thanks for playing!', 56, 38+guiY+VIRTUAL_HEIGHT)
+        love.graphics.print('this was my final project', 32, 54+guiY+VIRTUAL_HEIGHT)
+        love.graphics.print('for CS50 made in love2d', 40, 62+guiY+VIRTUAL_HEIGHT)
+        love.graphics.setColor(179/255, 48/255, 33/255, 1)
+        love.graphics.print('credits: nintendo', 62, 88+guiY+VIRTUAL_HEIGHT)
+        love.graphics.setColor(88/255, 225/255, 49/255, 1)
+        love.graphics.print('coded by djheffeson', 54, 104+guiY+VIRTUAL_HEIGHT)
+        love.graphics.setColor(1, 1, 1, 1)
+        love.graphics.print('yaay', endGame.animX-8, endGame.animY-6+guiY+VIRTUAL_HEIGHT)
+        endGame.anim:draw(sprites.linkSheet, endGame.animX, endGame.animY+2+guiY+VIRTUAL_HEIGHT)
+        love.graphics.print('press enter to exit', 52, 186+guiY+VIRTUAL_HEIGHT)
+    end
 end
